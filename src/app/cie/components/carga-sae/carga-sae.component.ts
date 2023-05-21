968f8579faf9b3185115e1ce124c7432d19810cb
Sebastian Flores <sfa120997@gmail.com>
2023-05-20 23:07:14 -0600

    XLSX File to JSON fn

--- src/app/cie/components/carga-sae/carga-sae.component.ts
+++ src/app/cie/components/carga-sae/carga-sae.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 interface Options {
   name:   string,
@@ -12,27 +13,71 @@ interface Options {
 })
 export class CargaSaeComponent implements OnInit {
 
-  fileSizeMax: number = 1000000;
-  isXlsx = true;
-  isLoadingFile = false;
+  excelData: any
+  jsonData: any = []
+
+  fileSizeMax: number = 1000000
+  isXlsx = true
+  isLoadingFile = false
   companyOptions: Options[] = [
     { name: 'Empresa 1', value: 'E1' },
     { name: 'Empresa 2', value: 'E2' }
-  ];
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  async onBasicUpload(event: any) {
-    if (event && event.files) {
-      this.isLoadingFile = true;
+  get jsonFormateado() {
+    return JSON.stringify(this.jsonData, null, 3)
+  }
 
-      setTimeout(() => {
-        console.log(event)
-      },3000);
+  async onBasicUpload(event: any) {
+    this.isLoadingFile = true
+  
+    console.log('load xlsx data...')
+    
+    const [ file ] = event.files
+    const fileReader = new FileReader()
+    fileReader.readAsBinaryString( file )
+
+    console.clear()
+    fileReader.onload = e => {
+      const workBook = XLSX.read( fileReader.result, { type: 'binary' } )
+      const [sheetName] = workBook.SheetNames
+      this.excelData = XLSX.utils.sheet_to_json( workBook.Sheets[sheetName] )
+      let counter = 0
+      let records: any [] = []
+      let tempRecords: any [] = []
+      let tempNormalRecords: any[] = []
+      let lastRecord = false
+      let isMiddleDash = false
+      this.excelData.map((record: any, i: number, row: any[]) => {
+        lastRecord = (i + 1) === row.length
+        isMiddleDash = record.Tipo === '-'
+        if(isMiddleDash || lastRecord) {
+          if(i > 0) {
+            records[counter - 1] = {
+              Cuenta:   tempRecords[counter - 1].__EMPTY,
+              datos:    tempNormalRecords.slice(0, tempNormalRecords.length - 1),
+              balance:  tempNormalRecords[tempNormalRecords.length - 1]
+            }
+            tempNormalRecords = []
+          }
+          if(!lastRecord)
+            tempRecords[counter++] = record
+          else
+            this.jsonData = {balance: record}
+        } else {
+          tempNormalRecords.push(record)
+        }
+      })
+      this.jsonData = {...this.jsonData, datos: records}
+      console.log(this.jsonData)
     }
+
+    this.isLoadingFile = false
   }
 
   onChangeCompany(event: any) {
