6b4e42d4758595edcabd0cc27ad4159a569f5ad9
Sebastian Flores <sfa120997@gmail.com>
2023-06-05 20:13:48 -0600

    Adjustment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  urlApiBovis: 'bovis-dev-api-up.azurewebsites.net',
+  urlApiBovis: 'https://bovis-dev-api-up.azurewebsites.net',
   redirectUri: 'https://brave-pond-01ea24c10.3.azurestaticapps.net',
   clientID: 'ba171311-b438-4c6a-ba16-28d9facb9e60',
   urlAuthority: 'https://login.microsoftonline.com/48986ced-d307-4c91-85be-933ccbcbaeb3'
