fbdef9d030437b0e6f9d3d5669bfb6a5ee913853
hunkabann <36930991+hunkabann@users.noreply.github.com>
2023-06-19 12:50:39 -0600

    Update environment.prod.ts
    jmmr

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   urlApiBovis: 'https://bovis-dev-api-up.azurewebsites.net/',
   redirectUri: 'https://witty-flower-05e9ea310.3.azurestaticapps.net',
-  clientID: 'ba171311-b438-4c6a-ba16-28d9facb9e60',
+  clientID: 'a2229dda-ff52-432a-9970-3e7b60a2b4c5',
   urlAuthority: 'https://login.microsoftonline.com/48986ced-d307-4c91-85be-933ccbcbaeb3'
 };
